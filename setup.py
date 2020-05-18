893306c97bff0526c42ee97dda789ebf310304be
Ana Custura <ana@netstat.org.uk>
2020-05-18 08:10:00 +0100

    fixup! Add install_requires to setup.py.

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@
 
 from setuptools import setup
 
+with open('requirements.txt') as f:
+    install_requires = f.readlines()
+
 setup(name='OnionPerf',
       version='0.2.pre',
       description='A utility to monitor, measure, analyze, and visualize the performance of Tor and Onion Services',
@@ -9,12 +12,5 @@ setup(name='OnionPerf',
       url='https://github.com/robgjansen/onionperf/',
       packages=['onionperf'],
       scripts=['onionperf/onionperf'],
-      install_requires=["lxml",
-                        "matplotlib",
-                        "networkx",
-                        "nose-cov",
-                        "nose",
-                        "numpy",
-                        "scipy",
-                        "stem"]
+      install_requires=install_requires
      )
